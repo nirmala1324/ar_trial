517eaee1184ccf62b757007bebb8b98af992be8c
unknown <nirmalapuspa1324@gmail.com>
2024-02-13 14:45:09 +0700

    cone example

---
+++ app/cone_example/page.js
@@ -0,0 +1,10 @@
+import ConeExample from "@/components/ConeExample";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <ConeExample />
+    </main>
+  );
+}
